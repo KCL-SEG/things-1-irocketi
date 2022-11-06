8371d15747b5940dcdcf2f7f0aeb52ce9397972f
Sabeeka Ahmad (k20012890) <sabeeka.1.ahmad@kcl.ac.uk>
2022-11-06 19:00:19 +0000

    edit

--- things/models.py
+++ things/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 # Create your models here.
 class Thing (models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=30)
     quantity = models.IntegerField()
-    description = models.CharField(max_length=500, blank=True)
+    description = models.CharField(max_length=120, blank=True)
